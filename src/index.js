1c12af25868f9278db27e2686087afbaa85a4e34
Srinvias <srinivas.gangji@gmail.com>
2019-03-09 10:57:03 -0700

    formatting updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-
 /*
-* Vue-Video-Player ssr.js
-* Author: surmon@foxmail.com
-* Github: https://github.com/surmon-china/vue-video-player
-* Adapted from Videojs (https://github.com/videojs/video.js)
-*/
+ * Vue-Video-Player ssr.js
+ * Author: surmon@foxmail.com
+ * Github: https://github.com/surmon-china/vue-video-player
+ * Adapted from Videojs (https://github.com/videojs/video.js)
+ */
 
-import _videojs from 'video.js'
-import videoPlayer from './player.vue'
+import _videojs from "video.js"
+import videoPlayer from "./player.vue"
 
 const videojs = window.videojs || _videojs
-const install = function (Vue, config) {
+const install = function(Vue, config) {
   if (config) {
     if (config.options) {
       videoPlayer.props.globalOptions.default = () => config.options
